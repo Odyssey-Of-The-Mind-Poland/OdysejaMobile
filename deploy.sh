15f18762134dc977acff4135ae8f8c40cfa074cb
grzybek <grzybol.k@gmail.com>
2022-03-30 23:05:21 +0200

    fixed deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 docker build -t grzybek/lappka .
 ssh grzybek docker kill $(ssh grzybek docker ps -q --filter ancestor=grzybek/lappka )
 docker save grzybek/lappka | bzip2 | pv | ssh grzybek docker load
-ssh grzybek docker run -p 1200:8081 -d grzybek/lappka
+ssh grzybek docker run -p 1201:80 -d grzybek/lappka
