715b8a0a60139f96cd618786c83f9a6e7a1bc129
grzybek <grzybol.k@gmail.com>
2022-03-30 23:02:10 +0200

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+docker build -t grzybek/lappka .
+ssh grzybek docker kill $(ssh grzybek docker ps -q --filter ancestor=grzybek/lappka )
+docker save grzybek/lappka | bzip2 | pv | ssh grzybek docker load
+ssh grzybek docker run -p 1200:8081 -d grzybek/lappka
