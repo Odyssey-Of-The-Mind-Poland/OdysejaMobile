ab2dd06e17c4eb66a8285ceb3ee019f56115146d
grzybek <grzybol.k@gmail.com>
2022-03-31 11:24:51 +0200

    click on whole Welcome screen

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 docker build -t grzybek/lappka .
 ssh grzybek docker kill $(ssh grzybek docker ps -q --filter ancestor=grzybek/lappka )
 docker save grzybek/lappka | bzip2 | pv | ssh grzybek docker load
-ssh grzybek docker run -p 1201:80 -d grzybek/lappka
+ssh grzybek docker run -p 9080:9080 --net=host -d grzybek/lappka
--- lib/presentation/main_view/initial_screens/welcome_screen.dart
+++ lib/presentation/main_view/initial_screens/welcome_screen.dart
@@ -30,52 +30,57 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
-    return Scaffold(
-        body: Padding(
-      padding: height > 700
-          ? const EdgeInsets.symmetric(horizontal: 40, vertical: 80)
-          : const EdgeInsets.symmetric(horizontal: 40, vertical: 30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Container(
-              height: 460,
-              alignment: Alignment.center,
-              child: AnimatedSwitcher(
-                  switchInCurve: Curves.easeInQuart,
-                  switchOutCurve: Curves.easeOutQuart,
-                  duration: AppValues.longerAnimationDuration,
-                  child: pages[_index])),
-          Padding(
-            padding: height > 700
-                ? const EdgeInsets.symmetric(vertical: 60)
-                : const EdgeInsets.symmetric(vertical: 40),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children:
-                  List.generate(pages.length, (index) => PositionIndicatorItem(_index == index)),
-            ),
-          ),
-          ElevatedButton(
-            // Debug override
-            // onPressed: !_isLast ? () => setState(() => ++_index) : () => setState(() => --_index),
-            onPressed: !_isLast
-                ? () => setState(() => ++_index)
-                : () => context.read<OnboardingBloc>().add(const FinishedOnboarding()),
-            child: AnimatedSwitcher(
-              switchInCurve: Curves.easeInQuart,
-              switchOutCurve: Curves.easeOutQuart,
-              duration: AppValues.longerAnimationDuration,
-              child: Text(
-                _isLast ? AppStrings.beginButtonLabel : AppStrings.nextButtonLabel,
-                key: Key('$_isLast'),
+    return GestureDetector(
+      onTap: !_isLast
+          ? () => setState(() => ++_index)
+          : () => context.read<OnboardingBloc>().add(const FinishedOnboarding()),
+      child: Scaffold(
+          body: Padding(
+        padding: height > 700
+            ? const EdgeInsets.symmetric(horizontal: 40, vertical: 80)
+            : const EdgeInsets.symmetric(horizontal: 40, vertical: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+                height: 460,
+                alignment: Alignment.center,
+                child: AnimatedSwitcher(
+                    switchInCurve: Curves.easeInQuart,
+                    switchOutCurve: Curves.easeOutQuart,
+                    duration: AppValues.longerAnimationDuration,
+                    child: pages[_index])),
+            Padding(
+              padding: height > 700
+                  ? const EdgeInsets.symmetric(vertical: 60)
+                  : const EdgeInsets.symmetric(vertical: 40),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:
+                    List.generate(pages.length, (index) => PositionIndicatorItem(_index == index)),
               ),
             ),
-          ),
-        ],
-      ),
-    ));
+            ElevatedButton(
+              // Debug override
+              // onPressed: !_isLast ? () => setState(() => ++_index) : () => setState(() => --_index),
+              onPressed: !_isLast
+                  ? () => setState(() => ++_index)
+                  : () => context.read<OnboardingBloc>().add(const FinishedOnboarding()),
+              child: AnimatedSwitcher(
+                switchInCurve: Curves.easeInQuart,
+                switchOutCurve: Curves.easeOutQuart,
+                duration: AppValues.longerAnimationDuration,
+                child: Text(
+                  _isLast ? AppStrings.beginButtonLabel : AppStrings.nextButtonLabel,
+                  key: Key('$_isLast'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
   }
 
   bool get _isLast => _index == pages.length - 1;
