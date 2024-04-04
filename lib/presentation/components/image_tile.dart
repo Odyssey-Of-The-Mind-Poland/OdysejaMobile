9db5d8be40611a2fab7b17e3afc3646d9274d238
Wojciech Błądek <wjbladek@gmail.com>
2024-04-04 08:03:22 +0200

    Add basic caching to sponsor graphics

--- lib/presentation/components/image_tile.dart
+++ lib/presentation/components/image_tile.dart
@@ -1,21 +1,23 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:odyssey_mobile/consts/themes.dart';
 
 class ImageTile extends StatelessWidget {
   const ImageTile(this.id, {this.color = AppColors.pureWhite, super.key});
-  final String id;
+  final int id;
   final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(AppValues.bigBrRadius),
-          image: DecorationImage(
-            image: NetworkImage('https://grzybek.snet.ovh/sponsor/$id'),
-            fit: BoxFit.scaleDown,
-          )),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(AppValues.bigBrRadius),
+      child: DecoratedBox(
+        decoration: BoxDecoration(color: color),
+        child: CachedNetworkImage(
+          imageUrl: 'https://grzybek.snet.ovh/sponsor/$id',
+          fit: BoxFit.contain,
+        ),
+      ),
     );
   }
 }
--- lib/presentation/home_screen/home_screen.dart
+++ lib/presentation/home_screen/home_screen.dart
@@ -1,7 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:odyssey_mobile/data/api/models/sponsor.dart';
-// import 'package:odyssey_mobile/consts/asset_paths.dart';
 import 'package:odyssey_mobile/consts/themes.dart';
 import 'package:odyssey_mobile/presentation/components/city_data_builder.dart';
 import 'package:odyssey_mobile/presentation/components/image_tile.dart';
@@ -15,12 +13,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return CityDataBuilder(onData: (data, _) {
-      return buildScaffold(size, data.sponsors);
-    });
-  }
-
-  Scaffold buildScaffold(Size size, List<List<SponsorModelApi>> sponsors) {
-    return Scaffold(
+      return Scaffold(
         backgroundColor: AppColors.shadowGrey,
         appBar: AppBar(title: const Text(AppStrings.homeScreenTitle), centerTitle: true),
         body: CustomScrollView(
@@ -32,7 +25,7 @@ class HomeScreen extends StatelessWidget {
                   Container(
                     margin: const EdgeInsets.only(left: 16.0, right: 16.0, top: 16),
                     height: size.width,
-                    child: ImageTile(sponsors[0][0].id.toString(), color: AppColors.omerBlue),
+                    child: ImageTile(data.sponsors[0][0].id, color: AppColors.omerBlue),
                   ),
                   const Padding(
                     padding: EdgeInsets.all(24.0),
@@ -45,7 +38,7 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
-            ...sponsors.skip(1).map((row) {
+            ...data.sponsors.skip(1).map((row) {
               return SliverPadding(
                 padding: const EdgeInsets.all(16),
                 sliver: SliverGrid(
@@ -58,7 +51,7 @@ class HomeScreen extends StatelessWidget {
                   delegate: SliverChildBuilderDelegate(
                     (BuildContext context, int index) {
                       if (index < row.length) {
-                        return ImageTile(row[index].id.toString());
+                        return ImageTile(row[index].id);
                       }
                       return null;
                     },
@@ -68,34 +61,8 @@ class HomeScreen extends StatelessWidget {
               );
             }),
           ],
-          // SliverToBoxAdapter(
-          //   child: Container(
-          //     margin: const EdgeInsets.all(16),
-          //     height: 120,
-          //     child: Row(children: const [
-          //       Expanded(child: ImageTile(AssetPaths.sponsor1)),
-          //       SizedBox(width: 16),
-          //       Expanded(child: ImageTile(AssetPaths.sponsor5)),
-          //     ]),
-          //   ),
-          // ),
-          // SliverPadding(
-          //   padding:
-          //       const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 16.0),
-          //   sliver: SliverGrid(
-          //     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          //       crossAxisCount: 3,
-          //       childAspectRatio: 1,
-          //       crossAxisSpacing: 16,
-          //       mainAxisSpacing: 16,
-          //     ),
-          //     delegate: SliverChildBuilderDelegate(
-          //       (context, index) => ImageTile(_imageGrid[index]!),
-          //       addAutomaticKeepAlives: false,
-          //       childCount: 12,
-          //     ),
-          //   ),
-          // ),
-        ));
+        ),
+      );
+    });
   }
 }
