f5913259b5b176f13acc05ef8d2c352a90f03cf1
Wojciech Błądek <wjbladek@gmail.com>
2024-04-04 09:04:57 +0200

    Add rudimentary selected day persistence

--- lib/presentation/components/schedule_layout.dart
+++ lib/presentation/components/schedule_layout.dart
@@ -8,7 +8,7 @@ import 'package:odyssey_mobile/presentation/components/favourites_list.dart';
 import 'package:odyssey_mobile/presentation/components/heading.dart';
 import 'package:odyssey_mobile/presentation/components/schedule_list.dart';
 
-class ScheduleLayout extends StatelessWidget {
+class ScheduleLayout extends StatefulWidget {
   const ScheduleLayout({
     super.key,
     required this.days,
@@ -31,61 +31,88 @@ class ScheduleLayout extends StatelessWidget {
   final Widget emptyWidget;
 
   @override
+  State<ScheduleLayout> createState() => _ScheduleLayoutState();
+}
+
+class _ScheduleLayoutState extends State<ScheduleLayout> with SingleTickerProviderStateMixin {
+  static int _initialIndex = 0;
+
+  late final TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(
+      length: widget.days.length,
+      vsync: this,
+      initialIndex: widget.days.length > 1 ? _initialIndex : 0,
+    );
+    _controller.addListener(() => _initialIndex = _controller.index);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final secretWidth = MediaQuery.of(context).size.width * AppValues.swipeTreshold - 24;
     // TODO convert into CustomScrollView and slivers?
-    final numDays = days.length;
+    final numDays = widget.days.length;
     if (numDays > 1) {
-      return DefaultTabController(
-        length: numDays,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text(title),
-            centerTitle: centerTitle,
-            actions: actions,
-            bottom: TabBar(tabs: days.map((e) => Text(e)).toList()),
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          centerTitle: widget.centerTitle,
+          actions: widget.actions,
+          bottom: TabBar(
+            controller: _controller,
+            tabs: widget.days.map((e) => Text(e)).toList(),
           ),
-          body: TabBarView(
-              physics: const NeverScrollableScrollPhysics(),
-              children: days
-                  .map(inFavourites
-                      ? (e) => FavouritesList(secretWidth: secretWidth, day: e)
-                      : (e) => ScheduleList(
-                            categoryEntity: categoryEntity,
-                            groups: performanceGroups.where((el) => el.day == e).toList(),
-                            secretWidth: secretWidth,
-                          ))
-                  .toList()),
+        ),
+        body: TabBarView(
+          controller: _controller,
+          physics: const NeverScrollableScrollPhysics(),
+          children: widget.days
+              .map(widget.inFavourites
+                  ? (e) => FavouritesList(secretWidth: secretWidth, day: e)
+                  : (e) => ScheduleList(
+                        categoryEntity: widget.categoryEntity,
+                        groups: widget.performanceGroups.where((el) => el.day == e).toList(),
+                        secretWidth: secretWidth,
+                      ))
+              .toList(),
         ),
       );
     } else if (numDays == 1) {
       return Scaffold(
-          appBar: AppBar(
-            title: Text(title),
-            centerTitle: centerTitle,
-            actions: actions,
+        appBar: AppBar(
+          title: Text(widget.title),
+          centerTitle: widget.centerTitle,
+          actions: widget.actions,
+        ),
+        body: Column(children: [
+          Heading(widget.days.first),
+          Expanded(
+            child: widget.inFavourites
+                ? FavouritesList(secretWidth: secretWidth)
+                : ScheduleList(
+                    categoryEntity: widget.categoryEntity,
+                    groups: widget.performanceGroups,
+                    secretWidth: secretWidth),
           ),
-          body: Column(
-            children: [
-              Heading(days.first),
-              Expanded(
-                child: inFavourites
-                    ? FavouritesList(secretWidth: secretWidth)
-                    : ScheduleList(
-                        categoryEntity: categoryEntity,
-                        groups: performanceGroups,
-                        secretWidth: secretWidth),
-              ),
-            ],
-          ));
+        ]),
+      );
     } else {
       return Scaffold(
         appBar: AppBar(
-          title: Text(title),
-          centerTitle: centerTitle,
-          actions: actions,
+          title: Text(widget.title),
+          centerTitle: widget.centerTitle,
+          actions: widget.actions,
         ),
-        body: emptyWidget,
+        body: widget.emptyWidget,
       );
     }
   }
