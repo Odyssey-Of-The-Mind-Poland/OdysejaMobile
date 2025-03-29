d840648458c2fb1674b5e951c4612f4aecda66d8
Wojciech Błądek <wjbladek@gmail.com>
2025-03-29 08:31:50 +0100

    Add logger to UrlLauncher

--- lib/core/data/services/url_service.dart
+++ lib/core/data/services/url_service.dart
@@ -1,13 +1,15 @@
+import 'package:odyssey_mobile/app/injectable.dart';
+import 'package:odyssey_mobile/core/data/services/logger_service.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-// TODO: Add error logging.
 abstract class UrlLauncher {
   static Future<bool> openUrl(String url) async {
     try {
       final uri = Uri.parse(url);
       final result = await launchUrl(uri, mode: LaunchMode.externalApplication);
       return result;
-    } catch (_) {
+    } catch (e, s) {
+      sl.maybeGet<LoggerService>()?.logError('Failed to launch an url', e, s);
       return false;
     }
   }
