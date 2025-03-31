3075bb1f6996dc97f850001e183220c49964243e
Wojciech Błądek <wjbladek@gmail.com>
2025-04-01 01:30:35 +0200

    Prepare for release; bump app version

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:odyssey_mobile/app/routing/router.dart';
@@ -6,7 +5,6 @@ import 'package:odyssey_mobile/features/update_data/bloc/update_bloc.dart';
 import 'package:odyssey_mobile/l10n/strings.dart';
 import 'package:odyssey_mobile/app/themes/theme_dark.dart';
 import 'package:odyssey_mobile/app/themes/theme_light.dart';
-import 'package:odyssey_mobile/app/themes/themes.dart';
 import 'package:odyssey_mobile/app/injectable.dart';
 
 class OdysseyMobile extends StatelessWidget {
@@ -26,9 +24,8 @@ class OdysseyMobile extends StatelessWidget {
         routeInformationParser: _appRouter.defaultRouteParser(),
         title: AppStrings.title,
         themeMode: ThemeMode.system,
-        // TODO: Remove [kDebugMode] flags after reaching full support of the new themes.
-        theme: kDebugMode ? themeLight : lightTheme,
-        darkTheme: kDebugMode ? themeDark : null,
+        theme: themeLight,
+        darkTheme: themeDark,
       ),
     );
   }
