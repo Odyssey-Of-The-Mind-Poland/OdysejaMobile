9ac3e1c6311816779f474004958a1705c4cb6e6b
Wojciech Błądek <wjbladek@gmail.com>
2025-04-01 21:28:21 +0200

    Implement info filter buttons and change info layout

--- lib/presentation/info_screen/info_grid.dart
+++ lib/presentation/info_screen/info_grid.dart
@@ -1,6 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:odyssey_mobile/core/domain/info_category.dart';
+import 'package:odyssey_mobile/core/domain/info.dart';
 import 'package:odyssey_mobile/presentation/components/basic_box.dart';
 import 'package:odyssey_mobile/app/themes/themes.dart';
 import 'package:odyssey_mobile/app/routing/router.dart';
@@ -8,24 +8,23 @@ import 'package:odyssey_mobile/app/routing/router.dart';
 import '../helpers/string_helpers.dart';
 
 class InfoGrid extends StatelessWidget {
-  const InfoGrid(this.infoGroup, {super.key});
-  final InfoCategory infoGroup;
+  const InfoGrid(this.info, {super.key});
+  final List<Info> info;
 
   @override
   Widget build(BuildContext context) {
-    var sorted = infoGroup.info;
-    sorted.sort((a, b) => b.sortNumber.compareTo(a.sortNumber));
     return GridView.count(
       crossAxisCount: 3,
       mainAxisSpacing: 16.0,
       crossAxisSpacing: 16.0,
       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
-      children: sorted
+      children: info
           .map((e) => BasicBox(
-              onTap: () => AutoRouter.of(context).navigate(InfoDetailRoute(info: e)),
-              label: StringHelper.removeOrphans(e.infoName),
-              centerLabel: true,
-              splashGradientPair: AppColors.safeSGPair(infoGroup.number)))
+                onTap: () => AutoRouter.of(context).navigate(InfoDetailRoute(info: e)),
+                label: StringHelper.removeOrphans(e.infoName),
+                centerLabel: true,
+                splashGradientPair: AppColors.safeSGPair(e.number + 2),
+              ))
           .toList(),
     );
   }
--- lib/presentation/info_screen/info_screen.dart
+++ lib/presentation/info_screen/info_screen.dart
@@ -1,9 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:odyssey_mobile/core/domain/info.dart';
+import 'package:odyssey_mobile/core/domain/info_category.dart';
 import 'package:odyssey_mobile/presentation/components/city_data_builder.dart';
 import 'package:odyssey_mobile/presentation/components/error_body.dart';
 import 'package:odyssey_mobile/presentation/info_screen/info_grid.dart';
 import 'package:odyssey_mobile/l10n/strings.dart';
+import 'package:odyssey_mobile/widgets/main_button.dart';
 import 'package:odyssey_mobile/widgets/top_bar.dart';
 
 @RoutePage()
@@ -14,23 +17,84 @@ class InfoScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return CityDataBuilder(
       onData: (data, _) {
-        return DefaultTabController(
-          length: data.infoGroups.length,
-          child: Scaffold(
+        // TODO: sort during data save.
+        data.infoGroups.sort(_sort);
+        data.infoGroups.forEach(_sortInfo);
+
+        int? selectedIndex;
+        return StatefulBuilder(builder: (context, setState) {
+          return Scaffold(
             appBar: TopBar(
               title: AppStrings.infoScreenTitle,
-              bottom: TabBar(
-                  tabs: data.infoGroups
-                      .map((e) => Text(e.name, textAlign: TextAlign.center))
-                      .toList()),
+              bottom: InfoFilterBar(
+                infoCategories: data.infoGroups,
+                selectedIndex: selectedIndex,
+                onFilterSelected: (value) {
+                  setState(() {
+                    if (selectedIndex == value) {
+                      selectedIndex = null;
+                      return;
+                    }
+                    selectedIndex = value;
+                  });
+                },
+              ),
             ),
-            body: TabBarView(children: data.infoGroups.map((e) => InfoGrid(e)).toList()),
-          ),
-        );
+            body: InfoGrid(_displayedInfoTiles(
+              categories: data.infoGroups,
+              selectedIndex: selectedIndex,
+            )),
+          );
+        });
       },
       onFailure: (failure) => Scaffold(
         appBar: TopBar(title: AppStrings.infoScreenTitle),
         body: ErrorBody(failure),
+      ),
+    );
+  }
+
+  _sortInfo(InfoCategory category) => category.info.sort(_sort);
+
+  int _sort(a, b) => a.number.compareTo(b.number);
+
+  List<Info> _displayedInfoTiles({int? selectedIndex, required List<InfoCategory> categories}) =>
+      selectedIndex != null
+          ? categories[selectedIndex].info
+          : categories.expand((e) => e.info).toList();
+}
+
+class InfoFilterBar extends StatelessWidget implements PreferredSizeWidget {
+  InfoFilterBar({
+    super.key,
+    required this.infoCategories,
+    required this.onFilterSelected,
+    required this.selectedIndex,
+  });
+
+  final List<InfoCategory> infoCategories;
+  final Function(int? value) onFilterSelected;
+  final int? selectedIndex;
+
+  static const _height = 68.0;
+
+  @override
+  final Size preferredSize = Size.fromHeight(InfoFilterBar._height);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: InfoFilterBar._height,
+      child: ListView.separated(
+        itemCount: infoCategories.length,
+        scrollDirection: Axis.horizontal,
+        padding: EdgeInsets.fromLTRB(16, 12, 16, 12),
+        separatorBuilder: (context, _) => SizedBox(width: 16),
+        itemBuilder: (context, i) => MainButton.tertiary(
+          label: infoCategories[i].name,
+          onPressed: () => onFilterSelected(i),
+          isSelected: selectedIndex == i,
+        ),
       ),
     );
   }
